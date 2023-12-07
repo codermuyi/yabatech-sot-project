121e03e46b1c272fa76f94d4c92294a1facb70d5
Oluwamuyiwa Samuel Adepoju <kasopeadeola@gmail.com>
2023-12-07 11:55:09 +0000

    chore: configured tailwindcss

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+
+const Navbar = () => {
+    return (
+        <div className="flex justify-between">
+            <div className="flex">
+                <div>
+                    <img src="/logo.svg" alt="Yabatech Logo" />
+                </div>
+                <div className="grid">
+                    <span>Yaba College of Technology</span>
+                    <span>P.M.B 2011,YABA, LAGOS. NIGERIA.</span>
+                </div>
+            </div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
+
+export default Navbar
